a014a6cfb47f5a9c3254e09862f24703ca03bd3c
Anton Gridushko <gridushko.anton@yahoo.com>
2025-02-07 21:07:37 +0300

    Добавь валидацию.

---
+++ api/servers-api/validation/IServerInstanceFluentValidator.cs
@@ -0,0 +1,10 @@
+﻿using servers_api.models.internallayer.instance;
+using servers_api.models.responces;
+
+namespace servers_api.validation
+{
+	public interface IServerInstanceFluentValidator
+	{
+		ResponceIntegration Validate(ServerInstanceModel instanceModel);
+	}
+}
---
+++ api/servers-api/validation/ServerInstanceFluentValidator.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using servers_api.models.internallayer.instance;
+using servers_api.models.responces;
+using servers_api.validation;
+
+public class ServerInstanceFluentValidator : IServerInstanceFluentValidator
+{
+	private readonly IValidator<ServerInstanceModel> _validator;
+	private readonly ILogger<ServerInstanceFluentValidator> _logger;
+
+	public ServerInstanceFluentValidator(
+		IValidator<ServerInstanceModel> validator,
+		ILogger<ServerInstanceFluentValidator> logger)
+	{
+		_validator = validator;
+		_logger = logger;
+	}
+
+	public ResponceIntegration Validate(ServerInstanceModel instanceModel)
+	{
+		ValidationResult result = _validator.Validate(instanceModel);
+
+		if (!result.IsValid)
+		{
+			foreach (var error in result.Errors)
+			{
+				_logger.LogError(error.ErrorMessage);
+			}
+
+			return new ResponceIntegration
+			{
+				Message = string.Join("; ", result.Errors.Select(e => e.ErrorMessage)),
+				Result = false
+			};
+		}
+
+		return null;
+	}
+}
